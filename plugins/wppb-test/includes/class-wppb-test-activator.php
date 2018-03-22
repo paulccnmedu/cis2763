83ea019a0ce4f9bfdaf9e18e6c3d9bb32408534a
root <root@paulc2763>
2018-03-22 16:50:01 +0000

    Backup

---
+++ plugins/wppb-test/includes/class-wppb-test-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       paulc.example.com
+ * @since      1.0.0
+ *
+ * @package    Wppb_Test
+ * @subpackage Wppb_Test/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wppb_Test
+ * @subpackage Wppb_Test/includes
+ * @author     Paul Clark <paulc@cnm.edu>
+ */
+class Wppb_Test_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ plugins/wppb-test/includes/class-wppb-test-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       paulc.example.com
+ * @since      1.0.0
+ *
+ * @package    Wppb_Test
+ * @subpackage Wppb_Test/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wppb_Test
+ * @subpackage Wppb_Test/includes
+ * @author     Paul Clark <paulc@cnm.edu>
+ */
+class Wppb_Test_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
