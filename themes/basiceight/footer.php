6c7675945dfbc5ecc1c9f2360fada800e3857d59
root <root@paulc2763>
2018-04-26 16:50:01 +0000

    Backup

---
+++ themes/basiceight/footer.php
@@ -0,0 +1,6 @@
+			<footer>
+				footer content here 
+			</footer>
+		</div> <!-- wrapper -->
+	</body>
+</html>
---
+++ themes/basiceight/sidebar.php
@@ -0,0 +1,6 @@
+
+	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+		<ul id="sidebar">
+			<?php dynamic_sidebar( 'sidebar-1'); ?>
+		</ul>
+	<?php endif; ?>
